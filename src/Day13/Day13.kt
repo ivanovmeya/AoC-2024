c184e6e1e4eeaa68f95978b0415433d469c5ab0a
Maksim Ivanov <ivanovmeya@gmail.com>
2024-12-13 14:14:19 +0700

    Day 13

--- src/Day13/Day13.kt
+++ src/Day13/Day13.kt
@@ -1,19 +1,76 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
+
+    fun parse(input: List<String>): List<Game> {
+        return input.filterNot { it.isEmpty() }.windowed(3, 3) { lines ->
+
+            val (a, b, price) = lines.map { line ->
+                val (xStr, yStr) = line.split(",")
+                val dx = xStr.substring(xStr.indexOfFirst { it.isDigit() }).trim().toInt()
+                val dy = yStr.substring(yStr.indexOfFirst { it.isDigit() }).trim().toInt()
+                Point(dx.toLong(), dy.toLong())
+            }
+            Game(
+                a = a,
+                b = b,
+                prize = price
+            )
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun calcTokens(games: List<Game>): Long {
+        return games.sumOf { g ->
+
+            val bTop = g.a.x * g.prize.y - g.a.y * g.prize.x
+            val bBottom = (g.a.x * g.b.y - g.a.y * g.b.x)
+
+            val timeBisWhole = bTop % bBottom == 0L
+
+            if (timeBisWhole) {
+                val timesB = bTop / bBottom
+                val aTop = (g.prize.x - g.b.x * timesB)
+                val aBottom = g.a.x
+                val timesA = aTop / aBottom
+                if (aTop % aBottom == 0L) {
+                    timesA * 3 + timesB
+                } else {
+                    0
+                }
+            } else {
+                0
+            }
+        }
+    }
+
+    fun part1(input: List<String>): Int {
+        val games = parse(input)
+        return calcTokens(games).toInt()
+    }
+
+    fun part2(input: List<String>): Long {
+        val games = parse(input)
+        val bigGames = games.map { game ->
+            Game(
+                game.a,
+                game.b,
+                Point(game.prize.x + 10000000000000, game.prize.y + 10000000000000)
+            )
+        }
+        return calcTokens(bigGames)
     }
 
     val testInput = readInput("Day13", true, 1)
-    check(part1(testInput) == 0)
+    check(part1(testInput) == 480)
 
     val testInput2 = readInput("Day13", true, 2)
-    check(part2(testInput2) == 0)
+    check(part2(testInput2) == 875318608908L)
 
     val input = readInput("Day13", false)
     part1(input).println()
     part2(input).println()
+}
+
+data class Game(val a: Point, val b: Point, val prize: Point) {
+    override fun toString(): String {
+        return "A:(${a.x},${a.y}); B:(${b.x},${b.y}); Prize = (${prize.x},${prize.y})"
+    }
 }
