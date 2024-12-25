f1baf4c3a27da618419b27539ecde88e7f2393e3
Maksim Ivanov <ivanovmeya@gmail.com>
2024-12-25 11:21:43 +0700

    Day 23

--- src/Day23/Day23.kt
+++ src/Day23/Day23.kt
@@ -1,17 +1,98 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
+
+    //adjacency list: node -> adjacent nodes
+    fun adjList(input: List<String>): Map<String, Set<String>> {
+        val res = mutableMapOf<String, MutableSet<String>>()
+        input.map {
+            it.split("-")
+        }.forEach {
+            val (n1, n2) = it
+            res[n1] = res.getOrDefault(n1, mutableSetOf()).apply {
+                add(n2)
+            }
+
+            res[n2] = res.getOrDefault(n2, mutableSetOf()).apply {
+                add(n1)
+            }
+        }
+        return res
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    //index is from 0 to allNodes.size
+    fun buildGroups(
+        node: String,
+        group: HashSet<String>,
+        adjs: Map<String, Set<String>>,
+        allGroups: HashSet<List<String>>
+    ) {
+        //to keep the group all interconnected.
+        //check if all group nodes in adjacent list of this node.
+        //if not - skip the node. if yes add the node, and continue.
+        val key = group.sorted()
+        if (allGroups.contains(key)) return
+        allGroups.add(key.toList())
+
+        for (adj in adjs[node]!!) {
+            if (group.contains(adj)) continue
+            if (!adjs[adj]!!.containsAll(group)) continue
+            group.add(adj)
+            buildGroups(adj, group, adjs, allGroups)
+        }
+    }
+
+    fun part1(input: List<String>): Int {
+        val adjs = adjList(input)
+
+        for (adj in adjs) {
+            println(adj)
+        }
+
+        println("Total keys = ${adjs.keys.size}")
+
+        val result = hashSetOf<List<String>>()
+
+        for (n1 in adjs.keys) {
+            for (n2 in adjs[n1]!!) {
+                for (n3 in adjs[n2]!!) {
+                    if (n1 != n3 && adjs[n3]!!.contains(n1)) {
+                        result.add(listOf(n1, n2, n3).sorted())
+                    }
+                }
+            }
+        }
+
+        println("Total groups = ${result.size}")
+        val filtered = result.filter { it.any { it.startsWith("t") } }
+        return filtered.size
+    }
+
+    fun part2(input: List<String>): String {
+        val adjs = adjList(input)
+
+        for (adj in adjs) {
+            println(adj)
+        }
+
+        val result = hashSetOf<List<String>>()
+        for (adj in adjs.keys) {
+
+            val group = hashSetOf<String>().apply {
+                add(adj)
+            }
+            buildGroups(adj, group, adjs, result)
+        }
+
+        println(result)
+
+        val maxBy = result.maxBy { it.size }
+        return maxBy.joinToString(",")
     }
 
     val testInput = readInput("Day23", true, 1)
-    check(part1(testInput) == 0)
-
     val testInput2 = readInput("Day23", true, 2)
-    check(part2(testInput2) == 0)
+
+    check(part1(testInput) == 7)
+    check(part2(testInput2) == "co,de,ka,ta")
 
     val input = readInput("Day23", false)
     part1(input).println()
